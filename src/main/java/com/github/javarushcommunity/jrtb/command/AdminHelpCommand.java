1de026ebd6b6e70fd2f9a6cba5d6c982c2563e95
edilk <ed2803k@gmail.com>
2022-11-29 20:08:19 +0600

    JRTB-10: extended bot statistics for admins.

---
+++ src/main/java/com/github/javarushcommunity/jrtb/command/AdminHelpCommand.java
@@ -0,0 +1,28 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static com.github.javarushcommunity.jrtb.command.CommandName.STAT;
+
+/**
+ * Admin help {@link Command}
+ */
+public class AdminHelpCommand implements Command {
+
+    public static final String ADMIN_HELP_MESSAGE = String.format("✨<b>Доступные команды админа</b>✨\n\n"
+                    + "<b>Получить статистику</b>\n"
+                    + "%s - статистика бота\n",
+            STAT.getCommandName());
+
+    private final SendBotMessageService sendBotMessageService;
+
+    public AdminHelpCommand(SendBotMessageService sendBotMessageService) {
+        this.sendBotMessageService = sendBotMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId(), ADMIN_HELP_MESSAGE);
+    }
+}
